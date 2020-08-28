1c2de14d75f81af57ace82587339e57e596665a2
imamfahr <imamfahr@gmail.com>
2020-08-28 20:33:40 +0800

    fix bugs for exercise 5

--- exercise5.js
+++ exercise5.js
@@ -4,9 +4,7 @@ let ask;
 while (answer) {
     ask = prompt("Sebutkan kepanjangan IB!");
     if (ask == 'impact byte') {
-        let answer = false
-      } else {
-        let answer = true}
+        answer = false;
+      }
     };
-
 alert("selamat jawaban anda benar");
